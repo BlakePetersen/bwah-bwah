b03e5b239c9a949f121aa5f13962fa0e977a33ec
Blake Petersen <blakepetersen@gmail.com>
2019-08-14 05:58:47 -0700

    remove getInitialProps to allow prerendering

--- components/page.js
+++ components/page.js
@@ -1,15 +1,16 @@
 import styled from 'styled-components';
 
 const Body = styled.div`
-  font-family: 'Hind', sans-serif;
-  -webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
+    font-family: 'Hind', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    touch-action: manipulation;
 `;
 
 const Page = ({ children }) => (
-    <Body>
-      { children }
-    </Body>
+	<Body>
+	    { children }
+	</Body>
 );
 
 export default Page
--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,6 @@ import App, { Container } from 'next/app'
 import GlobalStyles from './../styles/global-styles'
 
 export default class MyApp extends App {
-	static async getInitialProps ({ Component, router, ctx }) {
-		let pageProps = {};
-
-		if (Component.getInitialProps) {
-			pageProps = await Component.getInitialProps(ctx)
-		}
-
-		pageProps.pathname = router.route;
-		return { pageProps }
-	}
-
 	render () {
 		const {Component, pageProps} = this.props;
 
